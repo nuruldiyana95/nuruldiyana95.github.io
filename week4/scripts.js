895fafc61d71fb982da81f3941eed4d3fb4a26d5
nuruldiyana95 <nuruldiyanaameliahisman@gmail.com>
2020-12-04 20:39:07 +0800

    2341

--- week4/scripts.js
+++ week4/scripts.js
@@ -3,4 +3,18 @@ function transformUpperCase(){
     a.value = a.value.toUpperCase();
 }
 let elUsername = document.getElementById("namaPengguna");
-elUsername.addEventListener("keyup", transformUpperCase) ;
+elUsername.addEventListener("keyup", transformUpperCase);
+
+function checkPasswordLength(){
+    let password = document.getElementById("kataLaluan").value;
+    if(password.length < 5){
+        alert("Password must be more than 5 characters");
+    }else{
+            
+        
+        alert("Password accepted!")
+    }
+}
+
+let elPassword = document.getElementById("kataLaluan");
+elPassword.onblur = checkPasswordLength;
