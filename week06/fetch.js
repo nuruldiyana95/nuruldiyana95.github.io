7302bb445b50603a2ccd9966abb274bd23960439
nuruldiyana95 <nuruldiyanaameliahisman@gmail.com>
2020-12-29 18:04:33 +0800

    asada

--- week06/fetch.js
+++ week06/fetch.js
@@ -5,6 +5,17 @@ function GetBookings(){
         .then(json => {
             // Do something with the data
             console.log(json.sheet1S);
+            let bookingList = getElementById("bookingList");
+            bookingList.innerHTML = "";
+            for(let i=0; i < json.sheet1S.length;i++){
+                let gName = json.sheet1S[i].name;
+                let gEmail = json.sheet1S[i].email;
+                let gPax = json.sheet1S[i].pax;
+                let gId = json.sheet1S[i].id;
+
+                bookingList.innerHTML += gId + "-" + gName + ","+
+                gEmail + ", pax:" + pax + "<br>";
+            }
         });
 }
 
