a43005160aa1ce974f170b487b6dbc538e6de50f
nuruldiyana95 <nuruldiyanaameliahisman@gmail.com>
2021-01-02 19:13:09 +0800

    2323

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -1,10 +1,11 @@
 function GetBookings(){
-    let url = 'https://api.sheety.co/3f902d0ad0c8d4f2e64f2f7a3013751d/bookingApp/sheet1';
-    fetch(url)
-        .then((response) => response.json())
-        .then(json => {
-            // Do something with the data
-            console.log(json.sheet1S);
+    let url = 'https://api.sheety.co/3f902d0ad0c8d4f2e64f2f7a3013751d/bookingApp/bookings';
+fetch(url)
+.then((response) => response.json())
+.then(json => {
+  // Do something with the data
+  console.log(json.bookings);
+});
             let bookingList = getElementById("bookingList");
             bookingList.innerHTML = "";
             for(let i=0; i < json.sheet1S.length;i++){
